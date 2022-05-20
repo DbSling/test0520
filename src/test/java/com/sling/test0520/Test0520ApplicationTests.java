992d3bfc67d56e398f4a476de46ceadc66e2b8c2
용이다 <98091807+DbSling@users.noreply.github.com>
2022-05-20 17:45:11 +0900

    asd

---
+++ src/test/java/com/sling/test0520/Test0520ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sling.test0520;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0520ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
